fb7ae145e57f52268604fa2bf35755b9d28db668
unknown <vanzandt.school.email@gmail.com>
2023-12-18 15:40:13 -0500

    starter code

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+var timeEl = document.querySelector(".time");
+var mainEl = document.getElementById("main");
+
+let time = 60;
+
+function setTime() {
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      sendMessage();
+    }
+
+  }, 1000);
+}
+
+function sendMessage() {
+  timeEl.textContent = " ";
+  var imgEl = document.createElement("img");
+  imgEl.setAttribute("src", "images/image_1.jpg");
+  mainEl.appendChild(imgEl);
+
+}
+
+setTime();-
